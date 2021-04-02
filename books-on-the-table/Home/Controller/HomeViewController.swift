e74bb2119fcdfbb29c49634597d2a878c9093a73
administrator <administrator@8698.local>
2021-04-01 18:59:27 -0700

    Clean code

--- books-on-the-table/Home/Controller/HomeViewController.swift
+++ books-on-the-table/Home/Controller/HomeViewController.swift
@@ -25,8 +25,6 @@ class HomeViewController: UIViewController, UISearchBarDelegate, UITableViewData
         searchInput.delegate = self
         tableBooks.delegate = self
         tableBooks.dataSource = self
-//        booksCollectionView.dataSource = self
-//        booksCollectionView.delegate = self
     }
     
     // MARK: - SearchBar
@@ -74,46 +72,6 @@ class HomeViewController: UIViewController, UISearchBarDelegate, UITableViewData
         
         return headerView
     }
-    
-
-    // MARK: - CollectionView
-    
-//    func numberOfSections(in collectionView: UICollectionView) -> Int {
-////        return listBooksBySection.count
-//        return 1
-//    }
-    
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return listBooksBySection[section].books.count
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaLivro", for: indexPath) as! BookCollectionViewCell
-//        
-//        let books = listBooksBySection[indexPath.section].books
-//        let book = books[indexPath.row]
-//        
-//        cell.tituloLabel.text = book.title
-//        cell.autorLabel.text = book.author
-//        cell.layer.borderWidth = 0.5
-//        cell.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
-//        cell.layer.cornerRadius = 8
-//        
-//        return cell
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: collectionView.bounds.width / 3 - 15, height: 100)
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-//        let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SectionHeader", for: indexPath) as! SectionHeader
-//        
-//        sectionHeader.titleLabel.text = listBooksBySection[indexPath.section].title.uppercased()
-//        
-//        return sectionHeader
-//    }
-    
     
 }
 
